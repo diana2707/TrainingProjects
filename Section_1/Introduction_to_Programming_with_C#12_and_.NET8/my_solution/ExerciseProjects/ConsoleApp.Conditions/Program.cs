f8ef90b531d1d33b32a831fb3d894599eb696517
Diana Ruican <diana.ruican@endava.com>
2025-10-13 15:30:26 +0300

    implement complex if statement

--- Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.Conditions/Program.cs
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.Conditions/Program.cs
@@ -3,6 +3,11 @@
 Console.WriteLine("Please enter Student's grade: ");
 int grade = Convert.ToInt32(Console.ReadLine());
 
+// Simple If-else condition
+Console.WriteLine();
+Console.WriteLine("---------- Simple If-Else Condition ----------");
+Console.WriteLine();
+
 // Decide to print pass or fail based on input
 if (grade > 50)
 {
@@ -14,3 +19,38 @@ else
 }
 
 Console.WriteLine("End of program.");
+
+Console.WriteLine();
+Console.WriteLine("---------- End Simple If-Else Condition ----------");
+Console.WriteLine();
+
+// Complex If-else if condition
+Console.WriteLine();
+Console.WriteLine("---------- Complex If-Else if Condition ----------");
+Console.WriteLine();
+
+// Decide to print letter based on grade value
+if (grade < 0 || grade > 100)
+{
+    Console.WriteLine("Invalid grade entered.");
+}
+else if (grade < 50)
+{
+    Console.WriteLine("You have received an F grade.");
+}
+else if(grade >= 50 && grade <= 64)
+{
+    Console.WriteLine("You have received a C- grade.");
+}
+else if (grade >= 65 && grade <= 74)
+{
+    Console.WriteLine("You have received a C grade.");
+}
+else if (grade >= 85 && grade <= 100)
+{
+    Console.WriteLine("You have received a A grade.");
+}
+
+Console.WriteLine();
+Console.WriteLine("---------- End COmplex If-Else if Condition ----------");
+Console.WriteLine();
