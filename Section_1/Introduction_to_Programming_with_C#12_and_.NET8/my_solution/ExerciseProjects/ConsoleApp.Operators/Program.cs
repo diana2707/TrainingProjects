6e06a6a9e3669bb5ba916c84afa323c60bd28951
Diana Ruican <diana.ruican@endava.com>
2025-10-13 14:18:36 +0300

    implement math operators

---
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.Operators/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int num1 = 0;
+int num2 = 0;
+
+Console.WriteLine("Please enter he first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+// Add numbers
+int sum = num1 + num2;
+
+// Multiply numbers
+int product = num1 * num2;
+
+// Divide numbers
+int quotient = num1 / num2;
+
+// Subtract numbers
+int difference = num1 - num2;
+
+// Modulus
+int remainder = num1 % num2;
+
+Console.WriteLine($"The sum of {num1} and {num2} is: {sum}");
+Console.WriteLine($"The product of {num1} and {num2} is: {product}");
+Console.WriteLine($"The quotient of {num1} and {num2} is: {quotient}");
+Console.WriteLine($"The difference of {num1} and {num2} is: {difference}");
+Console.WriteLine($"The remainder of {num1} divided by {num2} is: {remainder}");
