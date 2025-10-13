31db090df93472c0137a5f3cd740e17ba844675a
Diana Ruican <diana.ruican@endava.com>
2025-10-13 15:11:02 +0300

    implement if statement

---
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.Conditions/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Prompt for input
+Console.WriteLine("Please enter Student's grade: ");
+int grade = Convert.ToInt32(Console.ReadLine());
+
+// Decide to print pass or fail based on input
+if (grade > 50)
+{
+    Console.WriteLine("You have passed the exam.");
+}
+else
+{
+    Console.WriteLine("You have failed the exam.");
+}
+
+Console.WriteLine("End of program.");
