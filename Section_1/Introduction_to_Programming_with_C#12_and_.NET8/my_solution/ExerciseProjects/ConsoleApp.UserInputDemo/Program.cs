9a748f36de74f6072d4bb6af93a579bd2e26bdce
Diana Ruican <diana.ruican@endava.com>
2025-10-13 11:58:00 +0300

    create different variable types and use user input to process data

---
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿// Declare variables
+string name = string.Empty;
+int age = 0;
+int retirementAge = 65;
+
+// Prompt user for input
+Console.Write("Enter your name: ");
+name = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+age = Convert.ToInt32(Console.ReadLine());
+
+// Process the data
+int yearsToRetirement = retirementAge - age;
+
+// Display the results
+Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Working years remaining: {yearsToRetirement}");
+
---
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Different data types
+/*
+    text - string
+    integer - int
+    decimal number - double, float, decimal
+    logical - bool
+ */
+
+string pet = "cat";
+
+Console.WriteLine(pet);
+Console.WriteLine($"I have a {pet}."); // string interpolation
+Console.WriteLine("My " + pet + " is orange."); // string concatenation
+Console.WriteLine("The {0} eats Wiskas.", pet); // composite formatting
+
+int age = 3;
+int averageLifespan = 15;
+Console.WriteLine($"My {pet} is {age} years old.");
+Console.WriteLine($"My {pet} will probably live for another {averageLifespan - age} years.");
+
+bool isHungry = true;
+Console.WriteLine($"Is my {pet} hungry? {isHungry}");
+
