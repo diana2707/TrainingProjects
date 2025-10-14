c9080c26667b38fdd82213402d9bce137de095da
diana2707 <61651284+diana2707@users.noreply.github.com>
2025-10-14 11:32:58 +0300

    Feat/chapter9 exceptions (#6)
    * implement try-catch
    
    * refactor calculator to throw exceptions
    
    ---------
    
    Co-authored-by: Diana Ruican <diana.ruican@endava.com>

---
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.ExceptionHandling/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Write a program that takes a user input age and prints it to the sceed
+// Display an error message if the input is invalid
+
+//Console.Write("Enter your age: ");
+//string? input = Console.ReadLine();
+
+//if (int.TryParse(input, out int result)){
+//    Console.WriteLine($"Your age is: {result}");
+//}
+//else{
+//    Console.WriteLine("Invalid input. Please enter a valid age.");
+//}
+
+try
+{
+    Console.Write("Enter your age: ");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Your age is: {age}");
+}
+catch (Exception)
+{
+    Console.WriteLine("Invalid input. Please enter a valid age.");
+    //throw;
+}
+finally
+{
+    Console.WriteLine("End");
+}
+
+
