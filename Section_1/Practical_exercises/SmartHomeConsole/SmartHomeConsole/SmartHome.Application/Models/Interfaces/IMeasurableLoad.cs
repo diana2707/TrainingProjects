4e9aa6989b80dc80092c2abe5775bf18865d4e01
Diana Ruican <diana.ruican@endava.com>
2025-10-14 15:00:17 +0300

    create IMeasurableLoad interface; modify SmartPlug class to implement it

---
+++ Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/Interfaces/IMeasurableLoad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Application.Models.Interfaces
+{
+    public interface IMeasurableLoad
+    {
+        public double CurrentWatts { get; }
+        public double TotalWh { get; }
+        public void ResetEnergy();
+    }
+}
--- Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/SmartPlug.cs
+++ Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/SmartPlug.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartHome.Application.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,20 @@ using System.Threading.Tasks;
 
 namespace SmartHome.Application.Models
 {
-    public class SmartPlug : SmartDevice
+    public class SmartPlug : SmartDevice, IMeasurableLoad
     {
         public override bool SelfTest()
         {
             Console.WriteLine("Performing self-test for SmartPlug...");
             return true;
+        }
+
+        public double CurrentWatts { get; private set; }
+        public double TotalWh { get; private set; }
+        public void ResetEnergy()
+        {
+            TotalWh = 0;
+            Console.WriteLine("Total energy consumption reset.");
         }
     }
 }
