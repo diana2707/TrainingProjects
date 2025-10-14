8ef98ce6dc5ba33133b95d24835fce32a790e8e0
diana2707 <61651284+diana2707@users.noreply.github.com>
2025-10-14 10:03:09 +0300

    Feat/chapter8 repetition statements (#5)
    * implement for and while loop statements
    
    * implement do while statement
    
    * enhance sample calculator to execute operations in a loop
    
    ---------
    
    Co-authored-by: Diana Ruican <diana.ruican@endava.com>

--- Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.SampleCalculator/Program.cs
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.SampleCalculator/Program.cs
@@ -1,45 +1,66 @@
 ﻿
 Console.WriteLine("---------- Welcome to the sample calculator! ----------");
 
-Console.Write("Please enter the first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Please enter the second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+int operation = 0;
 
-Console.WriteLine("Please select an operation:");
-Console.WriteLine("Addition (A)");
-Console.WriteLine("Subtraction (S)");
-Console.WriteLine("Multiplication (M)");
-Console.WriteLine("Division (D)");
-char operation = Convert.ToChar(Console.ReadLine().ToUpper());
-
-double result = 0.0;
-
-switch (operation)
+do
 {
-       case 'A':
-        result = num1 + num2;
-        break;
-    case 'S':
-        result = num1 - num2;
-        break;
-    case 'M':
-        result = num1 * num2;
-        break;
-    case 'D':
-        if (num2 != 0)
-        {
-            result = (double)num1 / num2; // Cast to double for accurate division
-        }
-        else
-        {
-            Console.WriteLine("Error: Division by zero is not allowed.");
-        }
-        break;
-    default:
-        Console.WriteLine("Invalid operation selected.");
-        break;
-}
+    Console.WriteLine("Please select an operation (-1 to exit):");
+    Console.WriteLine("Addition (1)");
+    Console.WriteLine("Subtraction (2)");
+    Console.WriteLine("Multiplication (3)");
+    Console.WriteLine("Division (4)");
+    Console.WriteLine("Accumulator (5)");
 
-Console.WriteLine($"The result is: {result}");
+    operation = Convert.ToInt32(Console.ReadLine());
+    
+    if (operation == -1)
+    {
+        break;
+    }
+
+    Console.Write("Please enter the first number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Please enter the second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    double result = 0.0;
+
+    switch (operation)
+    {
+        case 1:
+            result = num1 + num2;
+            break;
+        case 2:
+            result = num1 - num2;
+            break;
+        case 3:
+            result = num1 * num2;
+            break;
+        case 4:
+            if (num2 != 0)
+            {
+                result = (double)num1 / num2; // Cast to double for accurate division
+            }
+            else
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+            }
+            break;
+        case 5:
+            for (int i = num1; i < num2; i++)
+            {
+                result += i;
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid operation selected.");
+            break;
+    }
+
+    Console.WriteLine($"The result is: {result}");
+}
+while (operation != -1);
+
