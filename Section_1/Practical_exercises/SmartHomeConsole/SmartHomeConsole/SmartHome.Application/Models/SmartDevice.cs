dac03d7e1c650228a94c31762675831e3b4afb03
Diana Ruican <diana.ruican@endava.com>
2025-10-14 14:23:52 +0300

    create SmartDevice base class with properties Id, Name, IsOn, GetStatus

---
+++ Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/SmartDevice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Application.Models
+{
+    public abstract class SmartDevice
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+        
+        public bool IsOn { get; set; }
+
+        public string GetStatus {  get; set; }
+        public SmartDevice()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
