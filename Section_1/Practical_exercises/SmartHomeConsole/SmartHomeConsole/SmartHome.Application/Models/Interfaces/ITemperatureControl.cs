5b6fe154173453cbf88b6d150b6178dbeafd70f5
Diana Ruican <diana.ruican@endava.com>
2025-10-14 14:55:13 +0300

    create ITemperatureControl interface with TargetCelsius property and SetTarget(); modify Thermostat class to implement it

---
+++ Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/Interfaces/ITemperatureControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Application.Models.Interfaces
+{
+    public interface ITemperatureControl
+    {
+        double TargetCelsius { get; }
+        void SetTarget(double celsius);
+    }
+}
--- Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/Thermostat.cs
+++ Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/Thermostat.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartHome.Application.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,27 @@ using System.Threading.Tasks;
 
 namespace SmartHome.Application.Models
 {
-    public class Thermostat : SmartDevice
+    public class Thermostat : SmartDevice, ITemperatureControl
     {
+        public double TargetCelsius { get; private set; }
+
+        public void SetTarget(double celsius)
+        {
+            if (celsius < 10 || celsius > 30)
+            {
+                throw new ArgumentOutOfRangeException(nameof(celsius), "Target temperature must be between 10 and 30 degrees Celsius.");
+            }
+
+            TargetCelsius = celsius;
+            Console.WriteLine($"Target temperature set to: {TargetCelsius}°C.");
+        }
+
         public override bool SelfTest()
         {
             Console.WriteLine("Performing self-test for Thermostat...");
             return true;
         }
+
+
     }
 }
