d42265fe3de37749ab545abb8a7ef8edc9b9d635
Diana Ruican <diana.ruican@endava.com>
2025-10-14 11:31:59 +0300

    refactor calculator to throw exceptions

--- Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.SampleCalculator/Program.cs
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.SampleCalculator/Program.cs
@@ -4,63 +4,75 @@ Console.WriteLine("---------- Welcome to the sample calculator! ----------");
 
 int operation = 0;
 
-do
+while (operation != -1)
 {
-    Console.WriteLine("Please select an operation (-1 to exit):");
-    Console.WriteLine("Addition (1)");
-    Console.WriteLine("Subtraction (2)");
-    Console.WriteLine("Multiplication (3)");
-    Console.WriteLine("Division (4)");
-    Console.WriteLine("Accumulator (5)");
+    try {
+        Console.WriteLine("Please select an operation (-1 to exit):");
+        Console.WriteLine("Addition (1)");
+        Console.WriteLine("Subtraction (2)");
+        Console.WriteLine("Multiplication (3)");
+        Console.WriteLine("Division (4)");
+        Console.WriteLine("Accumulator (5)");
 
-    operation = Convert.ToInt32(Console.ReadLine());
+        operation = Convert.ToInt32(Console.ReadLine());
+      
+        if (operation == -1)
+        {
+            break;
+        }
+
+        int num1 = 0;
+        int num2 = 0;
+
+        Console.Write("Please enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Please enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        
+        double result = 0.0;
+
+        switch (operation)
+        {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                result = num1 / num2;
+                break;
+            case 5:
+                for (int i = num1; i < num2; i++)
+                {
+                    result += i;
+                }
+                break;
+            default:
+                throw new Exception("Invalid operation selected.");
+        }
+
+        Console.WriteLine($"The result is: {result}");
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Error: Division by zero is not allowed.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        
+    }
+    finally
+    {
+        Console.WriteLine("Press any key to try again");
+        Console.ReadKey();
+        Console.Clear();
+    }
     
-    if (operation == -1)
-    {
-        break;
-    }
-
-    Console.Write("Please enter the first number: ");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-
-    Console.Write("Please enter the second number: ");
-    int num2 = Convert.ToInt32(Console.ReadLine());
-
-    double result = 0.0;
-
-    switch (operation)
-    {
-        case 1:
-            result = num1 + num2;
-            break;
-        case 2:
-            result = num1 - num2;
-            break;
-        case 3:
-            result = num1 * num2;
-            break;
-        case 4:
-            if (num2 != 0)
-            {
-                result = (double)num1 / num2; // Cast to double for accurate division
-            }
-            else
-            {
-                Console.WriteLine("Error: Division by zero is not allowed.");
-            }
-            break;
-        case 5:
-            for (int i = num1; i < num2; i++)
-            {
-                result += i;
-            }
-            break;
-        default:
-            Console.WriteLine("Invalid operation selected.");
-            break;
-    }
-
-    Console.WriteLine($"The result is: {result}");
 }
-while (operation != -1);
 
