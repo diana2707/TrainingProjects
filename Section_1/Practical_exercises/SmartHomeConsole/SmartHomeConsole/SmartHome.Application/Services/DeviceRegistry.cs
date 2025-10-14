18819fb59f3ea6e3827cb949cf50d4b1edfe2b40
Diana Ruican <diana.ruican@endava.com>
2025-10-14 15:21:16 +0300

    create DeviceRegistry class

---
+++ Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Services/DeviceRegistry.cs
@@ -0,0 +1,36 @@
+﻿using SmartHome.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Application.Services
+{
+    public class DeviceRegistry
+    {
+        HashSet<SmartDevice> devices = [];
+
+        public void Add(SmartDevice device)
+        {
+            if (devices.Add(device))
+            {
+                Console.WriteLine($"Device {device.Name} is added to your smart home.");
+                return;
+            }
+            
+            Console.WriteLine($"Device {device.Name} is already in the registry.");
+        }
+
+        public void Remove(SmartDevice device)
+        {
+            if (devices.Remove(device))
+            {
+                Console.WriteLine($"Device {device.Name} removed from registry.");
+                return;
+            }
+
+            Console.WriteLine($"Device {device.Name} not found in the registry.");
+        }
+    }
+}
