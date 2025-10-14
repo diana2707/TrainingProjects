dd825f5a0f0844bfd21742fe3355e3fbb9c18935
Diana Ruican <diana.ruican@endava.com>
2025-10-14 14:37:31 +0300

    create IPowerSwitch interface with member PowerOn and PowerOff and modify SmartDevice class to implement it

--- Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/SmartDevice.cs
+++ Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/SmartDevice.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartHome.Application.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SmartHome.Application.Models
 {
-    public abstract class SmartDevice
+    public abstract class SmartDevice : IPowerSwitch
     {
         public Guid Id { get; private set; }
         public string Name { get; set; }
@@ -17,6 +18,16 @@ namespace SmartHome.Application.Models
         public SmartDevice()
         {
             Id = Guid.NewGuid();
+        }
+
+        public void PowerOn()
+        {
+            Console.WriteLine("Device is powered on.");
+        }
+
+        public void PowerOff()
+        {
+            Console.WriteLine("Device is powered off.");
         }
     }
 }
