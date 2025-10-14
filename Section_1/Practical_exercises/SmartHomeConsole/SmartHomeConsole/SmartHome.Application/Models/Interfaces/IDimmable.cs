9c8c43e505a45d3001505aa7ddad82354e19cd87
Diana Ruican <diana.ruican@endava.com>
2025-10-14 14:50:36 +0300

    create IDimmable interface with Brightness property and  SetBrightness method; modify LightBulb class to implement IDimmable interface

---
+++ Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/Interfaces/IDimmable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Application.Models.Interfaces
+{
+    public interface IDimmable
+    {
+        public int Brightness { get; }
+        public void SetBrightness(int value);
+    }
+}
--- Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/LightBulb.cs
+++ Section_1/Practical_exercises/SmartHomeConsole/SmartHomeConsole/SmartHome.Application/Models/LightBulb.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SmartHome.Application.Models.Interfaces;
 
 namespace SmartHome.Application.Models
 {
-    public class LightBulb : SmartDevice
+    public class LightBulb : SmartDevice, IDimmable
     {
         public override bool SelfTest()
         {
             Console.WriteLine("Performing self-test for LightBulb...");
             return true;
+        }
+
+        public int Brightness { get; private set; }
+
+        public void SetBrightness(int value)
+        {
+            if (value < 0 || value > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Brightness must be between 0 and 100.");
+            }
+
+            Brightness = value;
+            Console.WriteLine($"Brightness set to: {Brightness}%.");
         }
     }
 }
