ec4d459b91e19522ae5fe300d70cb248da678c05
diana2707 <61651284+diana2707@users.noreply.github.com>
2025-10-15 09:52:14 +0300

    Feat/chapter10 string and date time (#7)
    * implement string and DateTime examples
    
    * refactor UserInput project to use DateTime manipulations
    
    ---------
    
    Co-authored-by: Diana Ruican <diana.ruican@endava.com>

---
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.DateTimeManipulation/Program.cs
@@ -0,0 +1,99 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+//Console.WriteLine("---------- DateTime manipulations ----------");
+
+//// Empty DateTime
+//DateTime dateTime = new DateTime();
+
+//// Create a DateTime for a specific date
+//DateTime specificDate = new DateTime(2023, 10, 5, 14, 45, 25);
+//Console.WriteLine($"Specific Date: {specificDate}");
+
+//Console.WriteLine($"Year: {specificDate.Year}");
+//Console.WriteLine($"Month: {specificDate.Month}");
+//Console.WriteLine($"Day: {specificDate.Day}");
+//Console.WriteLine($"Time: {specificDate.TimeOfDay}");
+//Console.WriteLine($"Tick: {specificDate.Ticks}");
+//Console.WriteLine($"Kind: {specificDate.Kind}");
+
+//// Create a DateTime from current timestamp
+//DateTime currentDate = DateTime.Now;
+//Console.WriteLine($"Current Date: {currentDate}");
+
+//// Create a DateTime from a string
+//Console.WriteLine("Enter your DOB (MM/dd/yyyy): ");
+//string? input = Console.ReadLine();
+//var userDOB = DateTime.Parse(input);
+//Console.WriteLine($"Your DOB is: {userDOB}");
+
+//// Change format of DateTime
+//Console.WriteLine($"Formatted DOB: {userDOB:dd-MM-yyyy}");
+//Console.WriteLine($"Formatted DOB: {userDOB:dd, MM-yyyy}");
+
+//// Add additional time
+//Console.WriteLine($"One hour from now is: " + DateTime.Now.AddHours(1));
+
+//// Ticks from DateTime
+
+
+Console.WriteLine("---------- DateTime Offset ----------");
+
+// UTC = coordinated universal time
+var utcDateTime = DateTime.UtcNow;
+Console.WriteLine($"Now DateTime: {DateTime.Now}");
+Console.WriteLine($"UTC DateTime: {utcDateTime}");
+Console.WriteLine($"Local DateTime: {utcDateTime.ToLocalTime()}");
+
+// Time zones
+var tz = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow);
+Console.WriteLine($"Local Time Zone: {tz}");
+
+var dto = new DateTimeOffset(DateTime.Now, tz);
+Console.WriteLine($"DateTimeOffset: {dto}");
+Console.WriteLine($"DateTimeOffset UtcDateTime: {dto.UtcDateTime}");
+
+var indiaTz = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+var indiaDateTime = TimeZoneInfo.ConvertTime(DateTime.Now, indiaTz);
+
+var indiaDto = dto.ToOffset(indiaTz.GetUtcOffset(DateTime.UtcNow));
+Console.WriteLine($"India DateTimeOffset: {indiaDto}");
+
+
+// DateOnly
+var dateOnly = new DateOnly(1999, 12, 3);
+var nextDay = dateOnly.AddDays(1);
+var previousDay = dateOnly.AddDays(-1);
+var decadeLater = dateOnly.AddYears(10);
+var lastMonth = dateOnly.AddMonths(-1);
+
+Console.WriteLine($"DateOnly: {dateOnly}");
+Console.WriteLine($"Next Day: {nextDay}");
+Console.WriteLine($"Previous Day: {previousDay}");
+Console.WriteLine($"Decade Later: {decadeLater}");
+Console.WriteLine($"Last Month: {lastMonth}");
+
+var dateOnlyFromDateTime = DateOnly.FromDateTime(DateTime.Now);
+Console.WriteLine($"DateOnly from DateTime: {dateOnlyFromDateTime}");
+
+Console.WriteLine("What is your DOB (dd/MM/yyyy)");
+string? dobInput = Console.ReadLine();
+
+var userDob = DateOnly.ParseExact(dobInput, "dd/MM/yyyy",CultureInfo.InvariantCulture);
+Console.WriteLine($"Your DOB is: {userDob}");
+
+// TimeOnly
+var timeOnly = new TimeOnly(14, 30, 15);
+var timeFromDateTime = TimeOnly.FromDateTime(DateTime.Now);
+
+Console.WriteLine($"TimeOnly: {timeOnly}");
+Console.WriteLine($"TimeOnly from DateTime: {timeFromDateTime: hh:mm tt}");
+
+// Date comparisons
+var date1 = new DateOnly(2023, 10, 5);
+var date2 = new DateOnly(2023, 10, 10);
+
+Console.WriteLine($"{date1} is earlier than {date2}: {date1 < date2}");
+Console.WriteLine($"{date1} is later than {date2}: {date1 > date2}");
+Console.WriteLine($"{date1} is equal to {date2}: {date1 == date2}");
---
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.StringsAndDateTime/Program.cs
@@ -0,0 +1,78 @@
+﻿
+Console.WriteLine("---------- Strings manipulations ----------");
+
+// Initializa with a regular string literal
+string s1 = "This is a literal string";
+String s2 = "This is the literal string";
+
+// Declare without initialization - possible null exception
+string s3;
+
+// Initialize to null
+string? s4 = null;
+
+// Initialize an empty string
+string s5 = string.Empty;
+
+// Verbatim string literal
+string oldPath = "C:\\Users\\Public\\Documents";
+string path = @"C:\Users\Public\Documents";
+
+// Use a const string to prevent modification of a string
+const string constString = "This is a constant string";
+
+// Escape sequence of characters
+string sentence = "She said , \"Hello!\" \r\n Next line";
+
+// Raw string literals (for multiple lines)
+string rawString = """
+    This is a raw string literal.
+    It can span multiple lines.
+    It preserves whitespace and special characters like \n and \t.
+    """;
+
+// Review concatination and interpolation
+s1 = s1 + s2;
+s1 += " - Appended text";
+string concatString = string.Concat(s1, " - More text");
+string interpolatedString = $"{s1} - Interpolated text with value: {42}";
+string formattedString = string.Format("{0} - Formatted text with value: {1}", s1, 42);
+
+// String manipulation methods
+
+// Substrings
+string subString = s1.Substring(5);
+Console.WriteLine($"Substring from index 5: {subString}");
+
+subString = s1.Substring(5, 5);
+Console.WriteLine($"Substring from index 5 with length 5: {subString}");
+
+// Null or empty checks
+Console.WriteLine($"{nameof(s1)} has a lenght of {s1.Length}");
+
+//s4 is null
+if (!string.IsNullOrEmpty(s4))
+{
+    Console.WriteLine($"{nameof(s4)} has a lenght of {s4.Length}");
+}
+
+
+// Splitting strings
+var splitStrings = s1.Split(' ');
+for(int i = 0; i < splitStrings.Length; i++)
+{
+    Console.WriteLine($"Split part {i}: {splitStrings[i]}");
+}
+
+// Replace
+string replacements = s1.Replace("string", "STRING");
+
+// Convert to string
+string salary = 3000.ToString();
+int valueSalary = int.Parse(salary);
+
+
+// Changing formatting
+Console.WriteLine($"{nameof(salary)} : {salary:C}");
+Console.WriteLine(nameof(salary)+ ": " + valueSalary.ToString("C"));
+
