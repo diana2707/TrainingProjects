81275a769a3341168fdab8d9a1843b7b7b51cf75
Diana Ruican <diana.ruican@endava.com>
2025-10-15 11:47:26 +0300

    implement list exercises

---
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.Lists/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.WriteLine("---------- Lists ----------");
+
+// Declare a list
+List<int> grades = new List<int>();
+
+// Add values to the list
+//grades.Add(45);
+//Console.WriteLine(grades[0]);
+
+int grade = 0;
+
+do
+{
+    Console.Write("Enter grade (or -1 to exit): ");
+    grade = Convert.ToInt32(Console.ReadLine());
+    if (grade != -1)
+    {
+        grades.Add(grade);
+    }
+} while (grade != -1);
+
+
+
+// Print values of the list - for
+Console.WriteLine("Printing grades via for loop");
+for (int i = 0; i < grades.Count; i++)
+{
+    Console.WriteLine($"Grade {i + 1}: {grades[i]}");
+}
+
+
+// Print values in list - foreach
+Console.WriteLine("Printing grades via foreach loop");
+int count = 1;
+foreach (var g in grades)
+{
+    Console.WriteLine($"Grade {count}: {g}");
+    count++;
+}
