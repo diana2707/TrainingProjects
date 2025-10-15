5f551f63f0fd8923f5ed09ae9cccf9354f0c6013
Diana Ruican <diana.ruican@endava.com>
2025-10-15 11:34:29 +0300

    implement arrays examples

---
+++ Section_1/Introduction_to_Programming_with_C#12_and_.NET8/my_solution/ExerciseProjects/ConsoleApp.Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿
+Console.WriteLine("---------- Arrays ----------");
+
+// Declare fixed size array
+int[] grades = new int[5];
+
+
+// Add values to fixed size array
+//grades[0] = 45;
+//grades[1] = 25;
+//grades[2] = 38;
+//grades[3] = 45;
+//grades[4] = 54;
+
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.Write($"Enter grade for student {i + 1}: ");
+    grades[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+// Print values of fixed size array
+Console.WriteLine("Grades:");
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.WriteLine($"Student {i + 1}: {grades[i]}");
+}
+
+
+// Declare variable sized array
+string[] names = new string[] { "Alice", "Bob", "Charlie" };
+
+// Add value to variable sized array
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.Write($"Enter student name {i + 1}: ");
+    names[i] = Console.ReadLine();
+}
+
+// Print values of variable sized array
+Console.WriteLine("Names:");
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.WriteLine($"Student {i + 1}: {names[i]}");
+}
