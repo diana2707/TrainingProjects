77a67559db8cb92389787767ee74daab28a62c42
Diana Ruican <diana.ruican@endava.com>
2025-11-25 14:47:57 +0200

    create api project with EF

---
+++ Section_4/Ultimate_ASP.NET_Core_Web_API_Guide/HotelListing/HotelListing.API/Program.cs
@@ -0,0 +1,51 @@
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyHeader()
+                          .AllowAnyOrigin()
+                          .AllowAnyMethod());
+});
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration
+        .WriteTo.Console()
+        .ReadFrom.Configuration(context.Configuration);
+});
+
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+
+builder.Services.AddDbContext<HotelListingDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAllOrigins");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
