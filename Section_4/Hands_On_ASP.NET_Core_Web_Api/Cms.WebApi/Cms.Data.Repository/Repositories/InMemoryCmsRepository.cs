7655af3186ddd3d05fcbfba6e821951cbefa5c68
Diana Ruican <diana.ruican@endava.com>
2025-11-25 10:11:46 +0200

    apis exercises

---
+++ Section_4/Hands_On_ASP.NET_Core_Web_Api/Cms.WebApi/Cms.Data.Repository/Repositories/InMemoryCmsRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static Cms.Data.Repository.Course;
+
+namespace Cms.Data.Repository.Repositories
+{
+    public class InMemoryCmsRepository : ICmsRepository
+    {
+        List<Course> courses = null;
+        public InMemoryCmsRepository()
+        {
+            courses = new List<Course>
+            {
+                new Course()
+                {
+                    CourseId = 1,
+                    CourseName = "Computer Science",
+                    CourseDuration = 4,
+                    CourseType = COURSE_TYPE.Engineering
+                },
+
+                new Course()
+                {
+                    CourseId = 2,
+                    CourseName = "Information Technology",
+                    CourseDuration = 4,
+                    CourseType = COURSE_TYPE.Engineering
+                }
+            };
+        }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            return courses;
+        }
+
+        public async Task<IEnumerable<Course>> GetAllCoursesAsync()
+        {
+            return await Task.Run(() => courses.ToList());
+        }
+    }
+}
---
+++ Section_4/Hands_On_ASP.NET_Core_Web_Api/Cms.WebApi/Cms.Data.Repository/Repositories/SqlCmsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cms.Data.Repository.Repositories
+{
+    public class SqlCmsRepository //: ICmsRepository
+    {
+        public SqlCmsRepository() { }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            return null;
+        }
+    }
+}
